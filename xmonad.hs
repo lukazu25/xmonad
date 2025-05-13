84011784389683204b96a5384619eb8792b35216
Luka Zukhbaia <145780979+lukazu25@users.noreply.github.com>
2025-05-13 20:19:15 +0400

    Update xmonad.hs

--- xmonad.hs
+++ xmonad.hs
@@ -7,6 +7,7 @@ import XMonad.Layout.Spacing
 import XMonad.Layout.NoBorders
 import XMonad.Layout.Grid
 import XMonad.Layout.Tabbed
+import XMonad.Layout.ResizableTile
 import XMonad.Hooks.EwmhDesktops (ewmh, ewmhFullscreen)
 import System.IO
 import qualified XMonad.StackSet as W
@@ -36,16 +37,17 @@ myTabTheme = def
   , fontName            = "xft:Fira Code-10"
   }
 
--- Layouts
+-- Layouts 
 myLayouts = avoidStruts $
   tiledLayouts ||| full ||| grid ||| tabbedLayout
   where
     -- Apply spacing only to these layouts
     tiledLayouts = spacingRaw False (Border 24 4 4 4) False (Border 4 4 4 4) True $
--- add smartBorders tiled ||| smartBorders mirrorTiled if you don't want border on single window
                      tiled ||| mirrorTiled
 
-    tiled = Tall 1 (3/100) (1/2)
+-- add smartBorders tiled ||| smartBorders mirrorTiled if you don't want border on single window
+
+    tiled = ResizableTall 1 (3/100) (1/2) [] -- This is ResizableTile
     mirrorTiled = Mirror tiled
     full = Full
     grid = spacingRaw False (Border 24 4 4 4) False (Border 4 4 4 4) True Grid
@@ -73,7 +75,7 @@ main = do
         }
     , modMask = mod4Mask
     , terminal = "alacritty"
-    , borderWidth = 2
+    , borderWidth = 4
     , normalBorderColor = gruvboxBg0
     , focusedBorderColor = gruvboxYellow
     , workspaces = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
@@ -83,15 +85,30 @@ main = do
         spawn "pkill -u $USER dunst; dunst &"
         spawn "setxkbmap -layout us,ru,ge -option 'grp:alt_shift_toggle'"
     } `additionalKeys`
-    [ ((mod4Mask .|. shiftMask, xK_Return), spawn "alacritty")
+    [ -- Launch terminal
+      ((mod4Mask .|. shiftMask, xK_Return), spawn "alacritty")
+
+      -- Launch dmenu
     , ((mod4Mask, xK_p), spawn "dmenu_run -fn 'Fira Code-12' -nb '#282828' -nf '#ebdbb2' -sb '#d79921' -sf '#282828'")
+
+      -- Kill window
     , ((mod4Mask .|. shiftMask, xK_c), kill)
+
+      -- Sink window (put it into the tile layout)
     , ((mod4Mask, xK_t), withFocused $ windows . W.sink)
-    , ((mod4Mask, xK_comma), sendMessage (IncMasterN 1))
-    , ((mod4Mask, xK_period), sendMessage (IncMasterN (-1)))
+
+      -- Audio controls (volume and mute)
     , ((0, xF86XK_AudioLowerVolume), spawn "pamixer --decrease 5 && notify-send 'Volume' 'Decreased by 5%'")
     , ((0, xF86XK_AudioRaiseVolume), spawn "pamixer --increase 5 && notify-send 'Volume' 'Increased by 5%'")
     , ((0, xF86XK_AudioMute), spawn "pamixer --toggle-mute && notify-send 'Volume' 'Muted/Unmuted'")
+
+      -- Brightness controls
     , ((0, xF86XK_MonBrightnessUp), spawn "brightnessctl set +10% && notify-send 'Brightness' 'Increased by 10%'")
     , ((0, xF86XK_MonBrightnessDown), spawn "brightnessctl set 10%- && notify-send 'Brightness' 'Decreased by 10%'")
+
+      -- Resize window bindings for ResizableTile layout in all directions
+    , ((mod4Mask, xK_h), sendMessage Shrink)       -- Shrink window horizontally
+    , ((mod4Mask, xK_l), sendMessage Expand)       -- Expand window horizontally
+    , ((mod4Mask, xK_j), sendMessage MirrorShrink) -- Shrink window vertically
+    , ((mod4Mask, xK_k), sendMessage MirrorExpand) -- Expand window vertically
     ]
